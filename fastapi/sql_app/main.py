05af321b80a32ed4262aa422d3d669be48a48d27
Evan Agovino <evanagovino@MacBook-Air.local>
2025-08-20 14:12:32 -0400

    update routes

--- fastapi/sql_app/main.py
+++ fastapi/sql_app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 
 from . import models
 from .database import engine
-from .routes import mobile_app, web, v2
+from .routes import mobile_app, web
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -10,4 +10,3 @@ app = FastAPI()
 
 app.include_router(mobile_app.router)
 app.include_router(web.router)
-app.include_router(v2.router)
